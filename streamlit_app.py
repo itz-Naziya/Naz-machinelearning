4db30f189c67638976395d1ebc79a9a0b96ec6e6
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-14 23:31:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Machine learning app')
 
 st.info('This app builds a machine learning model')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
