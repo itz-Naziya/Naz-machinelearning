eb88d905a8398ecc9e5557822262ff71faf6fab3
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-15 12:27:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,10 @@ target_mapper = { 'Adelie' :0,
 def target_encode(val):
   return target_mapper[val]
 
+y=y_raw.apply(target_encode)
+y
+y_raw
+
 with st.expander('Input Features'):
   st.write('**Input penguin**')
   input_df
