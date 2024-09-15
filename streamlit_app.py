96715b2eaef2f3107b82b633432788c1d96d398b
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-15 10:53:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,21 @@ with st.sidebar:
   input_penguins = pd.concat([input_df , X ],axis=0)
 
 with st.expander('Input Features'):
-  st.write('**Input Penguins**')
+  st.write('**Input penguin**')
   input_df
-  st.write('**Combined Penguin Data**')
+  st.write('**Combined penguin Data**')
   input_penguins
+
+
+
+
+
+
+
+
+
+
+
+
+
+
