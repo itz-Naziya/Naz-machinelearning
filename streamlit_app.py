62bddad40543f8d18787817b95f78660f691b9cb
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-15 11:30:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ with st.expander('Input Features'):
   input_row
 
 #encode
-encode=['island', 'gender']
+encode=['island', 'sex']
 df_penguins=pd.get_dummies(input_penguins , prefix=encode)
 df_penguins[:1]
 
