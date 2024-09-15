da120b0ae5110ea71d87edb1a4a1dc5df0b4e0c3
itz-Naziya <meetnaziyaji@gmail.com>
2024-09-15 10:05:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,11 @@ with st.expander('Data Visualization'):
   #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
   st.scatter_chart(data=df, x= 'bill_length_mm',y='body_mass_g',color='species')
 
+#data preparations
+with st.sidebar:
+  st.header('Input Feautures') 
+  #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  island=st.selectbox('Island',('Biscoe','Torgersen','Dream') 
+  gender=st.selectbox('Gender',('male','female'))
+  bill_length_mm=st.slider('Bill length (mm)',32.1,59.6,43.9)
   
